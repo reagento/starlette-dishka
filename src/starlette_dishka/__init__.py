72e5535273e42a995190ceaf0e2b07f4c361bbac
chiri <chirizxc@proton.me>
2026-03-14 22:06:11 +0300

    bump dependencies and update ruff config

--- src/starlette_dishka/__init__.py
+++ src/starlette_dishka/__init__.py
@@ -1,14 +1,14 @@
-from dishka import FromDishka
-
-from .container import ContainerMiddleware, SyncContainerMiddleware
-from .integration import inject, setup_dishka
-from .provider import StarletteProvider
-
-__all__ = [
+__all__ = (
     "ContainerMiddleware",
     "FromDishka",
     "StarletteProvider",
     "SyncContainerMiddleware",
     "inject",
     "setup_dishka",
-]
+)
+
+from dishka import FromDishka
+
+from .container import ContainerMiddleware, SyncContainerMiddleware
+from .integration import inject, setup_dishka
+from .provider import StarletteProvider
